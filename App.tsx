eeb7ab15c17bdcf51ffaea6084cc75484d6dc85d
Sheng <sisheng1998@gmail.com>
2023-05-14 08:34:19 +0800

    setup routing

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Home from './app/screens/Home'
+
+type StackList = {
+  Home: undefined
+}
+
+const Stack = createNativeStackNavigator<StackList>()
 
 const App = () => {
   return (
-    <View className='flex-1 items-center justify-center bg-black'>
-      <Text className='text-white'>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ app/screens/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, SafeAreaView } from 'react-native'
+
+const Home = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center bg-black'>
+      <Text className='text-white'>
+        Open up App.js to start working on your app!
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
