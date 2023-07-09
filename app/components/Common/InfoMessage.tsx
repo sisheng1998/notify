e61d4fb8526f24c9267e83a13010ced9f3e59add
Sheng <sisheng1998@gmail.com>
2023-07-09 09:59:27 +0800

    update styling

--- app/components/Common/InfoMessage.tsx
+++ app/components/Common/InfoMessage.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 import InfoIcon from '../../icons/Info'
 
 const InfoMessage = ({ text }: { text: string }) => (
-  <View className='mb-2 flex-row items-center justify-start space-x-1.5 rounded-lg bg-primary/5 px-4 py-3'>
+  <View className='mb-2 flex-row items-center justify-center space-x-1.5 rounded-lg border border-primary bg-primary/5 p-4'>
     <InfoIcon className='h-5 w-5 text-primary' />
     <Text className='font-js-mid text-primary'>{text}</Text>
   </View>
